a490d88a66d7d13d54dc2ed7f96abdde198d6db9
Steve Conklin <sconklin@canonical.com>
2012-09-23 09:15:22 -0400

    Add license

--- dumper1.py
+++ dumper1.py
@@ -1,3 +1,15 @@
+#
+# Copyright (C) 2012 Steve Conklin
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation version 3.
+#
+# This program is distributed "as is" WITHOUT ANY WARRANTY of any kind,
+# whether express or implied; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
 import struct
 from sys import argv
 
--- img2stl.py
+++ img2stl.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2012 Steve Conklin
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation version 3.
+#
+# This program is distributed "as is" WITHOUT ANY WARRANTY of any kind,
+# whether express or implied; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
 
 from   STL            import STL
 import os
